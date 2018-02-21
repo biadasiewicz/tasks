024a141bc03f80b243e9f57657d955b3b84d08a4
tomasz <biadasiewicz@protonmail.com>
2018-02-21 14:02:04 +0100

    starting tasks suite

--- tasks/application.py
+++ tasks/application.py
@@ -1,6 +1,21 @@
+import pickle
+from .filesystem import Filesystem
+
+
+class TasksSuiteAlreadyActive(Exception):
+    pass
+
+
 class Application:
     def __init__(self, fs=None):
         self.fs = fs if fs else Filesystem()
 
     def is_tasks_suite_active(self):
         return self.fs.tasks_suite_path().exists()
+
+    def start_tasks_suite(self, ts):
+        if self.is_tasks_suite_active():
+            raise TasksSuiteAlreadyActive
+        else:
+            with open(self.fs.tasks_suite_path(), 'wb') as f:
+                pickle.dump(ts, f)
--- test/test_application.py
+++ test/test_application.py
@@ -2,7 +2,9 @@ import unittest
 import tempfile
 import shutil
 from tasks.filesystem import Filesystem
-from tasks.application import Application
+from tasks.application import Application, TasksSuiteAlreadyActive
+from tasks.tasks_suite import TasksSuite
+from tasks.task import Task
 
 
 class TestApplication(unittest.TestCase):
@@ -10,9 +12,18 @@ class TestApplication(unittest.TestCase):
         self.tempdir = tempfile.mkdtemp()
         self.fs = Filesystem(prefix=self.tempdir)
         self.app = Application(self.fs)
+        self.tasks_suite = TasksSuite()
+        self.tasks = [Task("1"), Task("2"), Task("3")]
 
     def tearDown(self):
         shutil.rmtree(self.tempdir)
 
     def test_is_tasks_suite_active(self):
         self.assertFalse(self.app.is_tasks_suite_active())
+        self.app.start_tasks_suite(self.tasks_suite)
+        self.assertTrue(self.app.is_tasks_suite_active())
+
+    def test_starting_already_active_tasks_suite(self):
+        self.app.start_tasks_suite(self.tasks_suite)
+        with self.assertRaises(TasksSuiteAlreadyActive):
+            self.app.start_tasks_suite(self.tasks_suite)
